808e56ad58cf7e12b73acc68d767f09e41753878
Spencer Gagnier <gagniesp@sheridancollege.ca>
2019-01-22 16:38:59 -0500

    third commit

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -14,6 +14,9 @@ public class StudentArray
     public static void main(String[] args)
     {
         System.out.println("hahaha");
+        
+        System.out.println("Third commit");
+        
         StudentDemo [] studentList = new StudentDemo[5];
         StudentDemo student1 = new StudentDemo("1", "Jim Halpern");
         studentList[0] = student1;
